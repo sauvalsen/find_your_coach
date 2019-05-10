f3b34fc3095777f8abb5aa723e16ef22b201adef
Stephane <gtadu76@hotmail.fr>
2019-05-10 12:21:08 +0200

    Mise à jour et corrections des erreurs des fixtures

--- src/DataFixtures/SportFixtures.php
+++ src/DataFixtures/SportFixtures.php
@@ -12,31 +12,31 @@ class SportFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-        $sport = new User();
+        $sport = new Sport();
         $sport->setSport('Foot');
         $manager->persist($sport);
 
-        $sport2 = new User();
+        $sport2 = new Sport();
         $sport2->setSport('Baseball');
         $manager->persist($sport2);
 
-        $sport3 = new User();
+        $sport3 = new Sport();
         $sport3->setSport('Judo');
         $manager->persist($sport3);
 
-        $sport4 = new User();
+        $sport4 = new Sport();
         $sport4->setSport('Basket');
         $manager->persist($sport4);
 
-        $sport5 = new User();
+        $sport5 = new Sport();
         $sport5->setSport('Yoga');
         $manager->persist($sport5);
 
-        $sport6 = new User();
+        $sport6 = new Sport();
         $sport6->setSport('Tennis');
         $manager->persist($sport6);
 
-        $sport7 = new User();
+        $sport7 = new Sport();
         $sport7->setSport('Karaté');
         $manager->persist($sport7);
 
