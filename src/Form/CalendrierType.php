97f07f6e231a7d83fdfbf119e3990b4707cf079b
Maxime <maxime.clement94@icloud.com>
2019-05-13 10:17:25 +0200

    Crud de base Calendrier

---
+++ src/Form/CalendrierType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Calendrier;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CalendrierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('start_date')
+            ->add('end_date')
+            ->add('id_coach')
+            ->add('id_sportif')
+            ->add('titre')
+       
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Calendrier::class,
+        ]);
+    }
+}
