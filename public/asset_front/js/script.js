b81e7e03141c5070bf625667966175e182cb1d8e
Anne <moulindelapierre@wanadoo.fr>
2019-05-17 17:18:50 +0200

    Recup av week-end

--- public/asset_front/js/script.js
+++ public/asset_front/js/script.js
@@ -1,8 +1,32 @@
 
-/*slicknav*/
-$(function(){
-  $('#menu').slicknav();
-})
+/*menu burger*/
+var content = document.querySelector('#hamburger-content');
+var sidebarBody = document.querySelector('#hamburger-sidebar-body');
+var button = document.querySelector('#hamburger-button');
+var overlay = document.querySelector('#hamburger-overlay');
+var activatedClass = 'hamburger-activated';
+
+sidebarBody.innerHTML = content.innerHTML;			
+
+button.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	this.parentNode.classList.add(activatedClass);
+});
+
+button.addEventListener('keydown', function(e) {
+	if (this.parentNode.classList.contains(activatedClass))
+	{
+		if (e.repeat === false && e.which === 27)
+			this.parentNode.classList.remove(activatedClass);
+	}
+});
+
+overlay.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	this.parentNode.classList.remove(activatedClass);
+});
 
 /*sticky navbar*/
 
