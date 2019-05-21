b593755b5950452f7e66cfc8eb616ec36450b50f
Stephane <gtadu76@hotmail.fr>
2019-05-21 17:41:11 +0200

    recherche

---
+++ src/Form/SportType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sport;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sport')
+            ->add('created_at')
+            ->add('modified_at')
+            ->add('search')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sport::class,
+        ]);
+    }
+}
